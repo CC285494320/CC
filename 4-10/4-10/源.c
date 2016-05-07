a099017442ffbd8f4e47ed932f428ccb0d7b087e
CC285494320 <285494320@qq.com>
2016-05-07 20:30:34 +0800

    r

---
+++ 4-10/4-10/源.c
@@ -0,0 +1,26 @@
+//1.每瓶汽水1元，两个空瓶可以置换一瓶汽水,
+//现在有20元，最多能喝多少瓶汽水。（编程实现）。
+#include<stdio.h>
+int main(){
+	int sum = 0;
+	int blank_bottle = 0;
+	int water_bottle = 20;
+	int flag = 0;
+	while (water_bottle>0){
+		sum += water_bottle;
+		blank_bottle = water_bottle+flag;
+		if (blank_bottle % 2 == 0){
+			water_bottle = blank_bottle / 2;
+			flag = 0;
+		}	
+		else{
+			water_bottle = blank_bottle / 2 ;
+			flag = 1;
+		}		
+	}
+	printf("sum=%d\n",sum);
+	system("pause");
+	return 0;
+}
+
+
