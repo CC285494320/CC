d65b75655e81edb49d30e521dd4cf69425fd4a43
CC285494320 <285494320@qq.com>
2016-05-10 11:43:39 +0800

    斐波那契数列

--- Feibonaxie/Feibonaqie/Feibonaxie.c
+++ Feibonaxie/Feibonaqie/Feibonaxie.c
@@ -1,24 +1,23 @@
-// ì³²¨ÄÇÆõÊıÁĞ
-//#include<stdio.h>
-//int Feibo(int value){
-//	int num1 = 1;
-//	int num2 = num1;
-//	int num3 = num1;
-//	if (value <= 0){
-//		return 0;
-//	}
-//	while (value >= 3){
-//		num3 = num1 + num2;
-//		num1 = num2;
-//		num2 = num3;
-//		value--;
-//	}
-//	return num3;
-//}
-//int main(){
-//	printf("num3 = %d\n", Feibo(-1));
-//	printf("num3 = %d\n", Feibo(0));
-//	printf("num3 = %d\n", Feibo(1));
-//	printf("num3 = %d\n", Feibo(3));
-//	return 0;
-//}
+#include<stdio.h>
+int Feibo(int value){
+	int num1 = 1;
+	int num2 = num1;
+	int num3 = num1;
+	if (value <= 0){
+		return 0;
+	}
+	while (value >= 3){
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		value--;
+	}
+	return num3;
+}
+int main(){
+	printf("num3 = %d\n", Feibo(-1));
+	printf("num3 = %d\n", Feibo(0));
+	printf("num3 = %d\n", Feibo(1));
+	printf("num3 = %d\n", Feibo(3));
+	return 0;
+}
