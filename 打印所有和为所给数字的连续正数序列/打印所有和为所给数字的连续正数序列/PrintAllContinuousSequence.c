148ade8c09005955efa3a344994548fc3d44d2d5
CC285494320 <285494320@qq.com>
2016-06-02 23:43:22 +0800

    打印所有和为所给数字的连续正数序列

---
+++ 打印所有和为所给数字的连续正数序列/打印所有和为所给数字的连续正数序列/PrintAllContinuousSequence.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void Print(int start, int end)
+{
+	for (; start <= end; start++)
+	{
+		printf("%3d",start);
+	}
+	printf("\n");
+}
+//int FindSum(int start,int end)
+//{
+//	int sum = 0;
+//	while (start <= end)
+//	{
+//		sum += start;
+//		start++;
+//	}
+//	return sum;
+//}
+void PrintAllContinuousSequence(int sum)
+{
+	int start = 1;
+	int end = 2;
+	int Allsum = start+end;
+	while (end <= (sum + 1) / 2) 
+	{
+		while (Allsum < sum)
+		{
+			end++;
+			Allsum += end;
+		}
+		if (Allsum == sum)
+		{
+			Print(start, end);
+			end++;
+			Allsum += end;
+		}
+		else
+		{
+			Allsum -= start;
+			start++;
+		}
+	}
+}
+
+int main()
+{
+	int sum = 9;
+	PrintAllContinuousSequence(sum);
+	return 0;
+}
